e22f4a27cf353f5a4b38750bb38ad122474d0a07
Taylor Rupe <personal@trupe.info>
2023-05-23 08:15:33 -0700

    Fixed height

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Inner from './InnerContainer'
 function App() {
 
   return (
-    <div className="bg-gray-800 py-8 px-4">
+    <div className="bg-gray-800 min-h-screen">
       <Inner />
     </div>
   )
